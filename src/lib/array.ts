89490be00b7bd07839ee70b3dc9b9afc7fe7566a
Sil van Diepen <me@sil.mt>
2022-01-10 21:38:45 +0100

    [test]: repeat function

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,7 +1,12 @@
-export const repeat = (num: number = 1, value: any) => {
+export const repeat = (
+  num: number = 1,
+  value: any,
+  toString = false
+): any[] | string => {
   let values = [];
   for (let i = 0; i < num; i++) {
     values.push(value);
   }
-  return values.join("");
+  if (toString) return values.join("");
+  else return values;
 };
